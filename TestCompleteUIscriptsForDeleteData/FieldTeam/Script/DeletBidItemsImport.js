067a88bdbd37299d186e3eac43d97b209ff12ef5
CMiCQA <130588318+CMiCQA@users.noreply.github.com>
2024-02-16 07:46:14 -0500

    Add files via upload

---
+++ TestCompleteUIscriptsForDeleteData/FieldTeam/Script/DeletBidItemsImport.js
@@ -0,0 +1,51 @@
+﻿//USEUNIT CommonFunctions
+
+function DeleteBidItemsImport()
+{
+  try{
+    CommonFunctions.LaunchURL(Project.Variables.Environment)
+//----------------------------------------------------------------- 
+    Aliases.Browser.PgeCmicR12LaunchPage.LnkProjectManagement.Click()
+    ObjEnvironmentPanel = Aliases.Browser.PgeCmicR12LaunchPage.PnlSelectAnEnvironment
+    StrEnv = Project.Variables.Environment[0].toUpperCase() + Project.Variables.Environment.slice(1).toLowerCase()
+    ObjEnvironmentPanel.FindChildByXPath("//a[text()='"+StrEnv+"']").Click()
+    Aliases.Browser.PgeCmicR12LaunchPage.BtnLaunchSoftware.Click()
+    Delay(3000, "Wait for page to load")
+//------------------------------------------------------------------  
+    CommonFunctions.Login(Project.Variables.Environment)
+//------------------------------------------------------------------ 
+  if (Aliases.Browser.PgeProjectManagement.frameTreeframe.LblBidItems.VisibleOnScreen == false){
+      Aliases.Browser.PgeProjectManagement.frameTreeframe.PnlBidManagement.Click()
+      }
+    Aliases.Browser.PgeProjectManagement.frameTreeframe.LblBidItems.Click()
+    Aliases.Browser.PgeProjectManagement.frameHeaderframe.LnkNavArrow.Click()
+    Aliases.Browser.PgeProjectManagement.frameHeaderframe.BtnShowimporteddata.Click()
+    Aliases.Browser.PgeShowImportedData.ChkBoxShowCurrentProj.Click()
+    Delay(3000, "Wait for page to load")
+//-----------------------------------------------------------------
+    var folderPath = aqFileSystem.ExcludeTrailingBackSlash(ProjectSuite.Path)
+    folderPath =  aqFileSystem.GetFileFolder(folderPath)
+    var filePath = folderPath + "\\TestData\\DataTransfer\\BidItemsDataTransfer.txt";
+    var file = Sys.OleObject("Scripting.FileSystemObject").OpenTextFile(filePath, 1); // 1 for reading
+    var fileContent = file.ReadLine();
+    ItemName = aqString.Unquote(fileContent)
+
+    objPnlCsvUploadRequestForm = Aliases.Browser.PgeShowImportedData.PnlCsvUploadRequestForm
+    objItemName = objPnlCsvUploadRequestForm.FindChildByXPath("//input[@value='"+ ItemName +"']//ancestor::td")
+    RowNum = objItemName.RowIndex
+    objCell = objPnlCsvUploadRequestForm.FindChild("Name","Cell("+RowNum+", 0)",30)
+    objCheckBox = objCell.FindChild("ObjectType","checkbox")
+    objCheckBox.Click()
+//-----------------------------------------------------------------
+    Aliases.Browser.PgeShowImportedData.BtnReValidate.Click()
+    Aliases.Browser.PgeShowImportedData.Confirm.buttonOk.Click()
+//------------------------------------------------------------------
+    CommonFunctions.CloseBrowser()
+  }
+//-----------------------------------------------------------------
+  catch(err)
+  {
+    Log.Warning("Exception: Error occured while deleting Bid Item Import "+err.description);
+  }
+}
+
---
+++ TestCompleteUIscriptsForDeleteData/FieldTeam/Script/DeleteSubContracts.js
@@ -0,0 +1,48 @@
+﻿//USEUNIT CommonFunctions
+
+function DeleteSubContracts()
+{
+  try{
+    CommonFunctions.LaunchURL(Project.Variables.Environment)
+//----------------------------------------------------------------- 
+    Aliases.Browser.PgeCmicR12LaunchPage.LnkProjectManagement.Click()
+    ObjEnvironmentPanel = Aliases.Browser.PgeCmicR12LaunchPage.PnlSelectAnEnvironment
+    StrEnv = Project.Variables.Environment[0].toUpperCase() + Project.Variables.Environment.slice(1).toLowerCase()
+    ObjEnvironmentPanel.FindChildByXPath("//a[text()='"+StrEnv+"']").Click()
+    Aliases.Browser.PgeCmicR12LaunchPage.BtnLaunchSoftware.Click()
+    Delay(3000, "Wait for page to load")
+//------------------------------------------------------------------  
+    CommonFunctions.Login(Project.Variables.Environment)
+//------------------------------------------------------------------ 
+  Delay(3000, "Wait for page to load")
+  if (Aliases.Browser.PgeProjectManagement.frameTreeframe.LnkSubContract.VisibleOnScreen == false){
+      Aliases.Browser.PgeProjectManagement.frameTreeframe.LblBudgetAndCostManagement.Click()
+      }
+    Aliases.Browser.PgeProjectManagement.frameTreeframe.LnkSubContract.Click()
+    Delay(3000, "Wait for page to load")
+//-----------------------------------------------------------------
+    var folderPath = aqFileSystem.ExcludeTrailingBackSlash(ProjectSuite.Path)
+    folderPath =  aqFileSystem.GetFileFolder(folderPath)
+    var filePath = folderPath + "\\TestData\\DataTransfer\\SubContractsDataTransfer.txt";
+    var file = Sys.OleObject("Scripting.FileSystemObject").OpenTextFile(filePath, 1); // 1 for reading
+    var fileContent = file.ReadLine();
+    ContractCode = aqString.Unquote(fileContent)
+//-----------------------------------------------------------------
+    Aliases.Browser.PgeProjectManagement.frameContentframe.TxtBoxSeachSC.SetText(ContractCode)
+    Sys.Keys("[Enter]")
+    Delay(3000, "Wait for page to load")
+    objTblSearchResultsSC = Aliases.Browser.PgeProjectManagement.frameContentframe.TblSearchResultsSC
+    objContractCode = objTblSearchResultsSC.FindChildByXPath("//td[contains(text(),'"+ContractCode+"')]")
+    objContractCode.Click()
+//-----------------------------------------------------------------
+    Aliases.Browser.PgeProjectManagement.frameHeaderframe.BtnDelete.Click()
+    Aliases.Browser.PgeProjectManagement.Confirm.Button("OK").Click()
+//-----------------------------------------------------------------
+    CommonFunctions.CloseBrowser()
+  }
+//-----------------------------------------------------------------
+  catch(err)
+  {
+    Log.Warning("Exception: Error occured while deleting SubContract "+err.description);
+  }
+}
